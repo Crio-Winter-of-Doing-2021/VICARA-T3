dc6d5027f25b4c1316b30928bab1a7a60beb1b6f
Paranjaysaxena <42527319+Paranjaysaxena@users.noreply.github.com>
2021-03-04 21:18:53 +0530

    add initial frontend setup 2

--- frontend/index.js
+++ frontend/index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT;
+app.use('/static', express.static('public'))
 
-// Homepage
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './templates/index.html'));
 });
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port);
-});
+app.listen(process.env.PORT);
