bb83338319f652f4797f9000948996b23666a6fa
Paranjaysaxena <42527319+Paranjaysaxena@users.noreply.github.com>
2021-03-05 12:05:06 +0530

    add login signup page

--- frontend/index.js
+++ frontend/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use('/static', express.static('public'))
+
+var cors = require('cors');
+
+app.use(cors());
+
+app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './templates/index.html'));
+});
+
+app.get('/drive', function (req, res) {
+    res.sendFile(path.join(__dirname, './templates/drive.html'));
 });
 
 app.listen(process.env.PORT);
