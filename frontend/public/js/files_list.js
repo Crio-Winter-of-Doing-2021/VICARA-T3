441d5774c0a974ee84b6335907044e5cd01863ad
ARIJIT ROY <arijitroyb@gmail.com>
2021-03-08 11:09:57 +0530

    code to get uploaded file list from backend added

---
+++ frontend/public/js/files_list.js
@@ -0,0 +1,18 @@
+async function getFileList() {
+    const xhttp = new XMLHttpRequest();
+    const backend_url = "http://localhost:5000/files";
+    xhttp.open("GET", backend_url, true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4) {
+            const response_status = xhttp.status;
+            if (response_status == 200) {
+                const file_list = this.responseText;
+                document.getElementById("files-list").innerHTML = file_list;
+            } else {
+                
+            }
+        }
+    };
+}
