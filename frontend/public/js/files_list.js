3d17eacac948c97daab681e1b3f6330b3bde7af4
ARIJIT ROY <arijitroyb@gmail.com>
2021-03-12 22:33:09 +0530

    added basic code to display files for a user

--- frontend/public/js/files_list.js
+++ frontend/public/js/files_list.js
@@ -1,18 +1,20 @@
-async function getFileList() {
-    const xhttp = new XMLHttpRequest();
-    const backend_url = "http://localhost:3000/files";
-    xhttp.open("GET", backend_url, true);
-    xhttp.send();
+async function getContent(file_list) {
+    let download_link = 'http://localhost:3000/download/:file_id'
+    console.log("inside getContent")
+    let len = file_list.length;
+    let content = ``;
+    for (let i = 0; i < len; i++) {
+        let file_details = file_list[i];
+        content += `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">${file_details['file_name']}</p>
+          <a href=${download_link} class="card-link">Download file</a>
+          <a href="#" class="card-link">Make Favourite</a>
+        </div>
+      </div>`
+    }
 
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4) {
-            const response_status = xhttp.status;
-            if (response_status == 200) {
-                const file_list = this.responseText;
-                document.getElementById("files-list").innerHTML = file_list;
-            } else {
-
-            }
-        }
-    };
+    return content;
 }
